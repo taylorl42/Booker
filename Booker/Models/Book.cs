1d594bfb44519818c1c9a2b3084ddf1323a69aa7
Taylor <Taylorl@rogers.com>
2019-05-22 18:23:42 -0400

    Created Book and Customer Models

---
+++ Booker/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booker.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string BookName { get; set; }
+
+        public string BookAuthor { get; set; }
+
+        public int YearOfPublication { get; set; }
+
+        public int NumberInStock { get; set; }
+
+        public int NumberRented { get; set; }
+    }
+}
