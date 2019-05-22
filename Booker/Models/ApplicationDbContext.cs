324d44d9661ab1c7732c63d1a2b49d9a06085643
Taylor <Taylorl@rogers.com>
2019-05-22 18:45:41 -0400

    Initialized Code-First Database with Books and Customer models

---
+++ Booker/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Booker.Models
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
