79ed2caa668228663c2c78eaa2d402259fbf9148
Taylor <Taylorl@rogers.com>
2019-06-04 15:13:30 -0400

    Populated Books Table

--- Booker/Models/Book.cs
+++ Booker/Models/Book.cs
@@ -9,7 +9,7 @@ namespace Booker.Models
     public class Book
     {
         public int Id { get; set; }
-
+        
         [Required]
         [StringLength(255)]
         public string Title { get; set; }
